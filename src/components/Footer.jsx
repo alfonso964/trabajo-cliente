fd57c49f7908d2a6fc530f439c76361cf2d575e2
Alfonso Reyes <alfonsoreyes656@gmail.com>
2026-02-24 13:30:07 +0100

    algunos detalles cambiados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,11 @@ function Footer() {
     <footer className="footer-main">
       <div className="footer-container">
         
-        {/* COLUMNA 1: LOGO Y FRASE */}
         <div className="footer-brand">
           <h2 className="footer-logo">ALFONSO <span>MOTORS</span></h2>
           <p>Líderes en la importación de vehículos de alta gama. Pasión por la ingeniería desde 2010.</p>
         </div>
 
-        {/* COLUMNA 2: ENLACES RÁPIDOS */}
         <div className="footer-links">
           <h4>Navegación</h4>
           <ul>
@@ -23,7 +21,6 @@ function Footer() {
           </ul>
         </div>
 
-        {/* COLUMNA 3: REDES SOCIALES */}
         <div className="footer-social">
           <h4>Síguenos</h4>
           <div className="social-icons">
